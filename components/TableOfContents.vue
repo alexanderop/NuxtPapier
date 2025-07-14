0ecd23ba0ea09bf71751067e3a3e2ef3933505b0
Alexander Opalic <opalic@gmx.net>
2025-07-14 19:00:21 +0200

    refactor: rename TocNode component to TableOfContentsNode and remove unused TocNode.vue
    - Updated import in TableOfContents.vue to use the new TableOfContentsNode.vue component.
    - Deleted the obsolete TocNode.vue file to streamline the codebase.
    
    This refactor improves clarity and maintainability by consolidating the table of contents component structure.

--- components/TableOfContents.vue
+++ components/TableOfContents.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import type { TocLink } from '@nuxt/content'
 import { tocKey } from './tocKey'
-import TocNode from './TocNode.vue'
+import TocNode from './TableOfContentsNode.vue'
 
 const { links = [] } = defineProps<{
   /** Array of table of contents links with hierarchy */
